017279a0fbbd31d3ac4a66361ab3ede94d370a5d
marsalan <marsalan@intellexalsolutions.com>
2020-10-03 05:46:53 +0500

    getting product from an array

---
+++ frontend/src/data.js
@@ -0,0 +1,29 @@
+export default {
+  products: [{
+    name: 'Slim Shirt',
+    category: 'Shirts',
+    image: '/images/d1.jpg',
+    price: 40,
+    brand: 'Nike',
+    rating: 4.3,
+    numReviews: 10
+  },
+  {
+    name: 'Fit Shirt',
+    category: 'Shirts',
+    image: '/images/d1.jpg',
+    price: 50,
+    brand: 'Nike',
+    rating: 4.3,
+    numReviews: 10
+  },
+  {
+    name: 'Best Pants',
+    category: 'Pants',
+    image: '/images/d1.jpg',
+    price: 60,
+    brand: 'Nike',
+    rating: 4.3,
+    numReviews: 10
+  }]
+}
